0bcea4bc1112a9d4e8c9de6aa3e28443d65a948e
Leandro <leandro.padulla@gmail.com>
2021-04-15 16:29:14 -0300

    Adpatado para rodar na minha maquina

--- main.py
+++ main.py
@@ -2,29 +2,31 @@
 import numpy as np
 import pandas as pd
 
+
+import sys
+sys.path.append('C:\\Users\\leand\\OneDrive\\Documentos\\GitHub\\padulla\\black_scholes')
+
 from utils import *
 import Call
 import Put
 
-FIXED_RATE = 3.40 / 100 # (1.6 - 3.98) / 100
+FIXED_RATE =  -3.863/ 100   #3.40 / 100 
 NOTIONAL = - 50_000_000
 
 def main():
-    data = pd.read_excel("Pasta1.xlsx", header = 1).iloc[0]
+    data = pd.read_excel("C:\\Users\\leand\\OneDrive\\Documentos\\GitHub\\padulla\\black_scholes\\Pasta1.xlsx", header = 1,engine='openpyxl').iloc[0]
     params = excel.extract_option_params_from_excel(data, FIXED_RATE)
 
-    c = Call.Call(**params)
+    #c = Call.Call(**params)
     p = Put.Put(**params)
-
-    p_dollar_delta = p.dollar_delta(NOTIONAL)
-    p_dollar_gamma = p.dollar_gamma(NOTIONAL)
-    p_dollar_theta = p.dollar_theta(NOTIONAL)
-    p_dollar_vega  = p.dollar_vega(NOTIONAL)
-    p_dollar_rho   = p.dollar_rho(NOTIONAL)
-
-    print("Delta = {:,.2f}\nGamma = {:,.2f}\nVega = {:,.2f}\nTheta = {:,.2f}\nRho = {:,.2f}".format(p_dollar_delta, p_dollar_gamma, p_dollar_vega, p_dollar_theta, p_dollar_rho))
+    print(params)
+    print("Delta = {:,.2f}".format(p.delta()))
+    print("Gamma = {:,.2f}".format(p.gamma()))
+    print("$Gamma_v2 = {:,.2f}".format(p.dollar_gamma(p.S)))
+    print("$Gamma = {:,.2f}".format(p.dollar_gamma(NOTIONAL)))
 
 
 if __name__ == "__main__": main()
+
 
 
